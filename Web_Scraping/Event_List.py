963a2c56d33a4bb3e332f102b0a1418a23f670e3
Malidrisi <mahalidrisi@gmail.com>
2017-03-06 15:37:32 -0500

    + date

--- Web_Scraping/Event_List.py
+++ Web_Scraping/Event_List.py
@@ -21,65 +21,72 @@ html = urllib2.urlopen(url).read().decode('utf8')
 soup = BeautifulSoup(html, 'html.parser')
 
 Event =soup.find_all("article",attrs={"class" : "clearfix events_list_wide_item"})
+Date = soup.find_all("h2",attrs={"class" : "events_list_day_date"})
 
 DL= "https://www.stevens.edu"
 
 Event_List = []
 
-for event in Event:
-    temp=[]
-    if event.get("class")[0] == u'clearfix':  
-
-        
-        # get category
-        category = event.find("span",attrs={"class" : "events_list_wide_item_cat"})
-        c=(category.get_text()).encode('ascii', 'ignore')
-        if len(c)>0:
-            temp+=[c]
+for date in Date:
+    for event in Event:
+        temp=[]
+        if date.get("class")[0] == u'events_list_day_date':  
+    
+            # get date
+            d = (date.get_text()).encode('ascii', 'ignore')
+            temp += [d]
+            
+            # get category
+            category = event.find("span",attrs={"class" : "events_list_wide_item_cat"})
+            c=(category.get_text()).encode('ascii', 'ignore')
+            if len(c)>0:
+                temp+=[c]
+            else:
+                temp+=["NA"]
+            # get title
+            title= event.find("h3",attrs={"class" : "events_list_wide_item_title"})
+            ti=(title.get_text()).encode('ascii', 'ignore')
+            temp+=[ti]
+            
+            # get time
+            time = event.find("time",attrs={"class" : "events_list_wide_item_time"})
+            t=(time.get_text()).encode('ascii', 'ignore')
+            temp+=[t]
+            
+            """
+            links=event.find("a",attrs={"class" :"events_list_wide_item_button events_list_wide_item_button_detail"})
+            l= links.get('href')
+            desclink= DL + l
+    
+            link= urllib2.Request(desclink,headers={'User-Agent': 'Safari/537.36'})
+    
+            h = urllib2.urlopen(link).read().decode('utf8')
+            s = BeautifulSoup(h, 'html.parser')
+    
+            desc =s.find("div",attrs={"class" : "field-item even"})
+            m=(desc.get_text()).encode('ascii', 'ignore')
+    
+            print m
+            
+            d=(desc.get_text()).encode('ascii', 'ignore')
+            temp+=[d]
+            """
+            # get location
+            Location=event.find("div",attrs={"class" : "fs-cell fs-md-2 fs-lg-4 events_list_wide_item_locations"})
+    
+            if len(Location) < 5:
+                temp+=["NA"]  
+            else:
+                m=event.find("div",attrs={"class" : "events_list_wide_item_location_data"})
+                loc=(m.get_text()).encode('ascii', 'ignore')
+                temp+=[loc]
         else:
-            temp+=["NA"]
-        # get title
-        title= event.find("h3",attrs={"class" : "events_list_wide_item_title"})
-        ti=(title.get_text()).encode('ascii', 'ignore')
-        temp+=[ti]
-        
-        # get date
-        time = event.find("time",attrs={"class" : "events_list_wide_item_time"})
-        t=(time.get_text()).encode('ascii', 'ignore')
-        temp+=[t]
-        
+            break
+    
+        Event_List=Event_List+[temp]
         """
-        links=event.find("a",attrs={"class" :"events_list_wide_item_button events_list_wide_item_button_detail"})
-        l= links.get('href')
-        desclink= DL + l
-
-        link= urllib2.Request(desclink,headers={'User-Agent': 'Safari/537.36'})
-
-        h = urllib2.urlopen(link).read().decode('utf8')
-        s = BeautifulSoup(h, 'html.parser')
-
-        desc =s.find("div",attrs={"class" : "field-item even"})
-        m=(desc.get_text()).encode('ascii', 'ignore')
-
-        print m
-        
-        d=(desc.get_text()).encode('ascii', 'ignore')
-        temp+=[d]
-        """
-        # get location
-        Location=event.find("div",attrs={"class" : "fs-cell fs-md-2 fs-lg-4 events_list_wide_item_locations"})
-
-        if len(Location) < 5:
-            temp+=["NA"]  
-        else:
-            m=event.find("div",attrs={"class" : "events_list_wide_item_location_data"})
-            loc=(m.get_text()).encode('ascii', 'ignore')
-            temp+=[loc]
-
-    Event_List=Event_List+[temp]
+        sql= "INSERT INTO Events (Eid, Ename, Elocation, Etime, Edate, Edescription, Eimage, Esource) values (1, 'John', 'Peterson', 'jp@example.com','abc123', 123456,null,'Email')"
+        con.commit()
+        # con.close()
     """
-    sql= "INSERT INTO Events (Eid, Ename, Elocation, Etime, Edate, Edescription, Eimage, Esource) values (1, 'John', 'Peterson', 'jp@example.com','abc123', 123456,null,'Email')"
-    con.commit()
-    # con.close()
-"""
 print Event_List
